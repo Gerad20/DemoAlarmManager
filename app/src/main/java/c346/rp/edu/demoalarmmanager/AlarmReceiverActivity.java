6655e02362dd240aca25835968fe52f32b44e6ff
18037522 <Chuat74@gmaol.com>
2020-05-29 10:09:03 +0800

    Initial commit

---
+++ app/src/main/java/c346/rp/edu/demoalarmmanager/AlarmReceiverActivity.java
@@ -0,0 +1,15 @@
+package c346.rp.edu.demoalarmmanager;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlarmReceiverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_receiver);
+    }
+}
